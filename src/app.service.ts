20aa3edab0db725b1d649841f2fb754ed1583824
Oybek-uzb <78894838+Oybek-uzb@users.noreply.github.com>
2022-06-02 01:32:54 +0500

    create-order.event added, billingClient emit done

--- src/app.service.ts
+++ src/app.service.ts
@@ -1,13 +1,20 @@
-import { Injectable } from '@nestjs/common';
+import {Inject, Injectable} from '@nestjs/common';
 import {CreateOrderRequestDto} from "./create-order-request.dto";
+import {ClientKafka} from "@nestjs/microservices";
+import {CreateOrderEvent} from "./create-order.event";
 
 @Injectable()
 export class AppService {
+  constructor(
+      @Inject('BILLING_SERVICE') private readonly billingClient: ClientKafka,
+  ) {
+  }
+
   getHello(): string {
     return 'Hello World!';
   }
 
-  createOrder(createOrderData: CreateOrderRequestDto) {
-
+  createOrder({ userId, price }: CreateOrderRequestDto) {
+    this.billingClient.emit('order_created', new CreateOrderEvent('123', userId, price))
   }
 }
